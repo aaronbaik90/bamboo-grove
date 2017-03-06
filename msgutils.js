d27b3d3e8ea94ab740aa604c58a0cd32ae3934a5
Doosan Baik <doosan.baik.90@gmail.com>
2017-03-05 20:40:18 -0500

    msgutils updated

--- msgutils.js
+++ msgutils.js
@@ -1,7 +1,31 @@
 module.exports = {
 
+sendTextMessage: function (recipientID, messageText) {
+  let messageData = {
+    recipient: {
+      id: recipientID,
+    },
+    message: {
+      text: messageText,
+    }
+  };
+  console.log('sending message now');
+},
+
 receivedMessage: function (event) {
-  console.log('Message data: ', event.message);
+  let senderID = event.sender.id;
+  let recipientID = event.recipient.id;
+  let timeOfMessage = event.timestamp;
+  let message = event.message;
+
+  let messageId = message.mid;
+  let messageText = message.text;
+  let messageAttachments = message.attachments;
+  if (messageText) {
+    sendTextMessage(senderID, messageText);
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
 }
 
 };
